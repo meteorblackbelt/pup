ecf34cdd19775648d06fe064cf9c5a031b3a278d
Angelo Berios <rubyblackbelt@angelo.ca>
2018-04-27 16:16:18 -0400

    Convert to react components

--- imports/ui/components/Public/Public.js
+++ imports/ui/components/Public/Public.js
@@ -2,29 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-const Public = ({
-  loggingIn, authenticated, afterLoginPath, component, path, exact, ...rest
-}) => (
-  <Route
-    path={path}
-    exact={exact}
-    render={props => (
-      React.createElement(component, {
-        ...props, ...rest
-      })
-    )}
-  />
-);
+class Public extends React.Component {
+  static get defaultProps() {
+    return {
+      path: '',
+      exact: false,
+    }
+  }
 
-Public.defaultProps = {
-  path: '',
-  exact: false,
-};
+  static get propTypes() {
+    return {
+      component: PropTypes.func.isRequired,
+      path: PropTypes.string,
+      exact: PropTypes.bool,
+    }
+  }
 
-Public.propTypes = {
-  component: PropTypes.func.isRequired,
-  path: PropTypes.string,
-  exact: PropTypes.bool,
-};
+  render() {
+    const { loggingIn, authenticated, afterLoginPath, component, path, exact, ...rest } = this.props;
+
+    return (
+      <Route
+        path={path}
+        exact={exact}
+        render={props => (
+          React.createElement(component, {
+            ...props,
+            ...rest
+          })
+        )}
+      />
+    );
+  }
+}
 
 export default Public;
--- imports/ui/components/Unauthenticated/Unauthenticated.js
+++ imports/ui/components/Unauthenticated/Unauthenticated.js
@@ -2,35 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Route, Redirect } from 'react-router-dom';
 
-const Unauthenticated = ({
-  loggingIn, authenticated, afterLoginPath, component, path, exact, ...rest
-}) => (
-  <Route
-    path={path}
-    exact={exact}
-    render={props => (
-      !authenticated ?
-        (React.createElement(component, {
-          ...props, ...rest, loggingIn, authenticated,
-        })) :
-        (<Redirect to={afterLoginPath || '/documents'} />)
-    )}
-  />
-);
+class Unauthenticated extends React.Component {
+  static get defaultProps() {
+    return {
+      path: '',
+      exact: false,
+      afterLoginPath: null,
+    }
+  }
 
-Unauthenticated.defaultProps = {
-  path: '',
-  exact: false,
-  afterLoginPath: null,
-};
+  static get propTypes() {
+    return {
+      loggingIn: PropTypes.bool.isRequired,
+      authenticated: PropTypes.bool.isRequired,
+      component: PropTypes.func.isRequired,
+      afterLoginPath: PropTypes.string,
+      path: PropTypes.string,
+      exact: PropTypes.bool,
+    }
+  }
 
-Unauthenticated.propTypes = {
-  loggingIn: PropTypes.bool.isRequired,
-  authenticated: PropTypes.bool.isRequired,
-  component: PropTypes.func.isRequired,
-  afterLoginPath: PropTypes.string,
-  path: PropTypes.string,
-  exact: PropTypes.bool,
-};
+  render() {
+    const { loggingIn, authenticated, afterLoginPath, component, path, exact, ...rest } = this.props;
+
+    return (
+      <Route
+        path={path}
+        exact={exact}
+        render={props => (
+          !authenticated ?
+            (React.createElement(component, {
+              ...props,
+              ...rest,
+              loggingIn,
+              authenticated,
+            })) :
+            (<Redirect to={afterLoginPath || '/documents'} />)
+        )}
+      />
+    );
+  }
+}
 
 export default Unauthenticated;
